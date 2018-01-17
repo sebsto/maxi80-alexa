322a61020c4108458aa13c04002bbe677c75ef94
Stormacq, Sebastien <stormacq@amazon.lu>
2018-01-17 13:40:15 +0100

    move jingle to maxi80 bucket

--- lambda/src/audioAssets.js
+++ lambda/src/audioAssets.js
@@ -2,18 +2,18 @@
 
 var audioData = {
 
-    card : {
+    card: {
         title: 'Maxi 80',
         subtitle: 'The best eighties music',
         //    subtitle: 'La radio de toute une génération',
-            cardContent: "Visit our web site https://www.maxi80.com",
-                image: {
+        cardContent: "Visit our web site https://www.maxi80.com",
+        image: {
             largeImageUrl: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/alexa-artwork-1200.png',
             smallImageUrl: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/alexa-artwork-720.png'
         }
     },
     url: 'https://audio1.maxi80.com',
-    startJingle : 'https://s3.amazonaws.com/alexademo.ninja/maxi80/jingle.m4a'    
+    startJingle: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/jingle.m4a'
 };
 
 module.exports = audioData;
