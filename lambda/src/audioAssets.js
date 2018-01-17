fcda211e4e9ae451a2b365a5bc19a0dbadfb09b6
Stormacq, Sebastien <stormacq@amazon.lu>
2018-01-17 13:39:56 +0100

    sync code with https://github.com/alexa/skill-sample-nodejs-audio-player/tree/mainline/single-stream

---
+++ lambda/src/audioAssets.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var audioData = {
+
+    card : {
+        title: 'Maxi 80',
+        subtitle: 'The best eighties music',
+        //    subtitle: 'La radio de toute une génération',
+            cardContent: "Visit our web site https://www.maxi80.com",
+                image: {
+            largeImageUrl: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/alexa-artwork-1200.png',
+            smallImageUrl: 'https://s3-eu-west-1.amazonaws.com/alexa.maxi80.com/assets/alexa-artwork-720.png'
+        }
+    },
+    url: 'https://audio1.maxi80.com',
+    startJingle : 'https://s3.amazonaws.com/alexademo.ninja/maxi80/jingle.m4a'    
+};
+
+module.exports = audioData;
--- lambda/src/audioEventHandlers.js
+++ lambda/src/audioEventHandlers.js
@@ -1,15 +1,14 @@
 'use strict';
 
-var Alexa = require('alexa-sdk');
 var audioData = require('./audioAssets');
-var constants = require('./constants');
+var controller = require('./audioController.js');
 
-// Binding audio handlers to PLAY_MODE State since they are expected only in this mode.
 var audioEventHandlers =  {
     'PlaybackStarted' : function () {
         /*
          * AudioPlayer.PlaybackStarted Directive received.
          * Confirming that requested audio file began playing.
+         * Do not send any specific response.
          */
         console.log("Playback started");        
         this.emit(':responseReady');
@@ -18,34 +17,37 @@ var audioEventHandlers =  {
         /*
          * AudioPlayer.PlaybackFinished Directive received.
          * Confirming that audio file completed playing.
+         * Do not send any specific response.
          */
         console.log("Playback finished");
-        this.response.audioPlayerClearQueue('CLEAR_ENQUEUED');
         this.emit(':responseReady');
     },
     'PlaybackStopped' : function () {
         /*
          * AudioPlayer.PlaybackStopped Directive received.
          * Confirming that audio file stopped playing.
-         * Storing details in dynamoDB using attributes.
          */
         console.log("Playback stopped");
 
         //do not return a response, as per https://developer.amazon.com/docs/custom-skills/audioplayer-interface-reference.html#playbackstopped
         this.emit(':responseReady');
-        //this.callback(null, null);
     },
     'PlaybackNearlyFinished' : function () {
+        /*
+         * AudioPlayer.PlaybackNearlyFinished Directive received.
+         * Replacing queue with the URL again.
+         * This should not happen on live streams
+         */
         console.log("Playback nearly finished");
-        // this.response.audioPlayerClearQueue('CLEAR_ENQUEUED');
-        this.response.audioPlayerPlay('REPLACE_ALL', audioData.url, audioData.url, null, 0);
-        this.emit(':responseReady');
+        controller.playLater.call(this, audioData.url);
     },
     'PlaybackFailed' : function () {
-        //  AudioPlayer.PlaybackNearlyFinished Directive received. Logging the error.
+        /*
+         * AudioPlayer.PlaybackFailed Directive received.
+         * Logging the error and restarting playing.
+         */
         console.log("Playback Failed : %j", this.event.request.error);
-        this.response.audioPlayerClearQueue('CLEAR_ENQUEUED');
-        this.emit(':responseReady');
+        controller.clear.call(this);
     }
 };
 
