6a346cd749c2eb493b5ef962494fb856dcf9e317
Stormacq, Sebastien <stormacq@amazon.lu>
2018-04-19 18:18:39 +0200

    add support for skill events

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -5,18 +5,21 @@ import { RequestEnvelope, ResponseEnvelope } from 'ask-sdk-model';
 import { IntentHandler } from './IntentHandlers';
 import { AudioHandler } from './AudioHandlers';
 import { RadioRequestHandler } from './utils/RadioRequestHandler';
+import { SkillEventHandler } from './SkillEventHandler';
 
 import { Constants } from './Constants';
 
-export async function handler(event : RequestEnvelope, context : any, callback : any) : Promise<void> {
+export async function handler(event: RequestEnvelope, context: any, callback: any): Promise<void> {
     const factory = SkillBuilders.custom()
-    .addRequestHandlers(RadioRequestHandler.builder()
-        .withHandlers(IntentHandler)
-        .withHandlers(AudioHandler)
-    .build());
+        .addRequestHandlers(new SkillEventHandler(),
+            RadioRequestHandler.builder()
+                .withHandlers(IntentHandler)
+                .withHandlers(AudioHandler)
+                .build()
+        );
 
     const skill = factory.create();
-    
+
     try {
 
         if (Constants.debug) {
@@ -24,18 +27,18 @@ export async function handler(event : RequestEnvelope, context : any, callback :
             console.log(JSON.stringify(event, null, 2));
         }
 
-        const responseEnvelope : ResponseEnvelope = await skill.invoke(event, context);
-        
+        const responseEnvelope: ResponseEnvelope = await skill.invoke(event, context);
+
         if (Constants.debug) {
             console.log("\n" + "******************* RESPONSE  **********************");
             console.log(JSON.stringify(responseEnvelope, null, 2));
         }
-        
+
         return callback(null, responseEnvelope);
 
     } catch (error) {
         if (Constants.debug) {
-            console.log(JSON.stringify(error, null,2 ));
+            console.log(JSON.stringify(error, null, 2));
         }
         return callback(error);
     }
